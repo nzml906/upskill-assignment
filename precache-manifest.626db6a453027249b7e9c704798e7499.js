446533a657fdd79d9bcd8ca27176d84be50a421d
Nazmul <grey000906@gmail.com>
2020-09-13 06:28:57 +0600

    deploy

---
+++ precache-manifest.626db6a453027249b7e9c704798e7499.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36beb97a9ce6a9751f3b",
+    "url": "/upskill-assignment/css/app.fe67a554.css"
+  },
+  {
+    "revision": "c5d8737db6c90393ace1d3bf3f0111f1",
+    "url": "/upskill-assignment/imgs/c1.png"
+  },
+  {
+    "revision": "5e063e7fb404ccd224d65b6b7939c88d",
+    "url": "/upskill-assignment/imgs/c2.png"
+  },
+  {
+    "revision": "1402936e5842d21ed986250471897b19",
+    "url": "/upskill-assignment/imgs/c3.png"
+  },
+  {
+    "revision": "a6db864b0a6a8a35cb962446322f1794",
+    "url": "/upskill-assignment/imgs/c4.png"
+  },
+  {
+    "revision": "e08bd093373a770691f03228e29a061d",
+    "url": "/upskill-assignment/imgs/c5.png"
+  },
+  {
+    "revision": "50d95cf013dd8a2c19c3ad6571c9ee66",
+    "url": "/upskill-assignment/imgs/footer.png"
+  },
+  {
+    "revision": "183cbe532fb087ba5ef8f3bd8aa8d597",
+    "url": "/upskill-assignment/imgs/hero.png"
+  },
+  {
+    "revision": "9de406e86a7c88c9ce7e1a0e3b71672e",
+    "url": "/upskill-assignment/imgs/logo.png"
+  },
+  {
+    "revision": "3a134d0109bf81efed4c2bcf20ae2a4f",
+    "url": "/upskill-assignment/index.html"
+  },
+  {
+    "revision": "36beb97a9ce6a9751f3b",
+    "url": "/upskill-assignment/js/app.a5b1c825.js"
+  },
+  {
+    "revision": "7efa97dc2965ff51ff3d",
+    "url": "/upskill-assignment/js/chunk-vendors.a84bc62a.js"
+  },
+  {
+    "revision": "9df58162eb17af9e657ed5394af61b6d",
+    "url": "/upskill-assignment/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/upskill-assignment/robots.txt"
+  }
+]);
